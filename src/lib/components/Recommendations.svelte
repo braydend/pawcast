3ee5967312a98c9df94647cc97584809d2e91a2d
Brayden Di Luca <bray.diluca@estimateone.com>
2023-12-19 06:26:38 +0800

    Split air and ground temperature recommendations

--- src/lib/components/Recommendations.svelte
+++ src/lib/components/Recommendations.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { getColourForGrade } from "$lib/colours";
 	import { temperatureLimits } from "$lib/domain/dogSafety";
-	import { getTemperatureRecommendations } from "$lib/domain/recommendation";
+	import { getRecommendationMessages } from "$lib/domain/recommendation";
 	import Paw from "$lib/icons/Paw.svelte";
 	import * as Card from "$lib/shadcn/ui/card";
 	import type { ForecastRecord, Grade } from "$lib/types";
@@ -12,17 +12,17 @@
     const RECOMMENDATION_GRADES_TO_INCLUDE = ["warning","danger","extreme"];
 
     type Recommendation = {
-        type: "UV"|"temperature";
+        type: "UV"|"air-temperature"|"ground-temperature";
         subject: "dog"|"human";
         grade: Grade;
         timeStart: number;
         timeEnd: number;
     };
 
-    const recommendationCollections = forecasts.map<Recommendation>((forecast) => {
+    const gradedForecasts = forecasts.map<Recommendation>((forecast) => {
         if (forecast.temperature < temperatureLimits.safe) {
             return {
-                type: "temperature", 
+                type: "air-temperature", 
                 subject: "dog",
                 grade: "safe",
                 timeStart: forecast.time,
@@ -31,7 +31,7 @@
         }
         if (forecast.temperature < temperatureLimits.warning) {
             return {
-                type: "temperature", 
+                type: "air-temperature", 
                 subject: "dog",
                 grade: "warning",
                 timeStart: forecast.time,
@@ -40,7 +40,7 @@
         }
         if (forecast.temperature < temperatureLimits.danger) {
             return {
-                type: "temperature", 
+                type: "air-temperature", 
                 subject: "dog",
                 grade: "danger",
                 timeStart: forecast.time,
@@ -48,7 +48,7 @@
             }
         }
         return {
-                type: "temperature", 
+                type: "air-temperature", 
                 subject: "dog",
                 grade: "extreme",
                 timeStart: forecast.time,
@@ -57,17 +57,40 @@
     })
     .filter(({ grade}) => RECOMMENDATION_GRADES_TO_INCLUDE.includes(grade))
     .toSorted(({timeStart: a}, {timeStart: b}) => a<b?0:1)
-    .reduce<Recommendation[]>((acc, cur) => {
-        const prev = acc.at(-1);
-        if (acc.length === 0 || !prev) {
+    ;
+
+    const airTemperatureRecommendations = gradedForecasts
+        .reduce<Recommendation[]>((acc, cur) => {
+            const prev = acc.at(-1);
+            if (acc.length === 0 || !prev) {
+                return [...acc, cur];
+            }
+            if (prev.grade === cur.grade) {
+                const rest = acc.slice(0,-1);
+                return [...rest, {...prev, timeEnd: cur.timeEnd}];
+            }
             return [...acc, cur];
-        }
-        if (prev.grade === cur.grade) {
-            const rest = acc.slice(0,-1);
-            return [...rest, {...prev, timeEnd: cur.timeEnd}];
-        }
-        return [...acc, cur];
-    }, new Array<Recommendation>());
+        }, new Array<Recommendation>());
+
+    const groundTemperatureRecommendations = gradedForecasts
+        .reduce<Recommendation[]>((acc, cur) => {
+            const currentGround: Recommendation = {
+                ...cur, 
+                type: "ground-temperature",
+            };
+            const prev = acc.at(-1);
+            if (acc.length === 0 || !prev) {
+                return [...acc, currentGround];
+            }
+            if (prev.timeEnd + 3600 === currentGround.timeStart) {
+                const rest = acc.slice(0,-1);
+                return [...rest, {...prev, timeEnd: currentGround.timeEnd}];
+            }
+            return [...acc, currentGround];
+        }, new Array<Recommendation>());
+
+    const allRecommendations = [...airTemperatureRecommendations, ...groundTemperatureRecommendations]
+        .toSorted(({timeStart: a}, {timeStart: b}) => a<b?0:1);
 
     //TODO: Cannot dynamically assemble classnames, so this is the current workaround
 	// These colours reference the values in the colours.ts file
@@ -87,14 +110,14 @@
 </script>
 <section class="col-start-1 col-span-2 gap-4 flex flex-col">
     <h3>Recommendations</h3>
-    {#each recommendationCollections as recommendation}
+    {#each allRecommendations as recommendation}
         <Card.Root class={borderColours[gradeToColour(recommendation.grade)]}>
             <Card.Header>
                 <Paw strokeColour={getColourForGrade(recommendation.grade)} />
                 <h4>{dayjs.unix(recommendation.timeStart).format('h:mm a D MMM')} - {dayjs.unix(recommendation.timeEnd).endOf("hour").format('h:mm a D MMM')}</h4>
             </Card.Header>
             <Card.Content>
-                {#each getTemperatureRecommendations(recommendation.grade) as message}
+                {#each getRecommendationMessages(recommendation.grade, recommendation.type) as message}
                     <p>{message}</p>
                 {/each}
             </Card.Content>
--- src/lib/domain/recommendation.ts
+++ src/lib/domain/recommendation.ts
@@ -6,3 +6,28 @@ export const getTemperatureRecommendations = (grade: Grade) => {
 		Boolean(msg)
 	);
 };
+
+export const getRecommendationMessages = (
+	grade: Grade,
+	type: 'UV' | 'ground-temperature' | 'air-temperature' | 'all'
+) => {
+	let messages: (string | undefined)[];
+
+	switch (type) {
+		case 'UV':
+			messages = []; // TODO: Build out UV recommendations
+			break;
+		case 'air-temperature':
+			messages = [airTemperatureMessages[grade]];
+			break;
+		case 'ground-temperature':
+			messages = [groundTemperatureMessages[grade]];
+			break;
+		case 'all':
+		default:
+			messages = [groundTemperatureMessages[grade], airTemperatureMessages[grade]];
+			break;
+	}
+
+	return messages.filter((msg) => Boolean(msg));
+};
