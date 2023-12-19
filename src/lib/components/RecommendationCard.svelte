994da6df2defa377aed8232e71b331ea2ba1929c
Brayden Di Luca <bray.diluca@estimateone.com>
2023-12-19 17:37:01 +1100

    Add Recommendation card component

---
+++ src/lib/components/RecommendationCard.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	import { getColourForGrade } from "$lib/colours";
+	import { getRecommendationMessages } from "$lib/domain/recommendation/recommendation";
+	import type { Recommendation } from "$lib/domain/recommendation/types";
+	import Paw from "$lib/icons/Paw.svelte";
+	import Sun from "$lib/icons/Sun.svelte";
+	import Thermometer from "$lib/icons/Thermometer.svelte";
+	import * as Card from "$lib/shadcn/ui/card";
+	import type { Grade } from "$lib/types";
+	import dayjs from "dayjs";
+
+    export let recommendation: Recommendation;
+
+    // TODO: Cannot dynamically assemble classnames, so this is the current workaround
+	// These colours reference the values in the colours.ts file
+	const borderColours = {
+		red: "border-[#dc2626]",
+		yellow: "border-[#fde047]",
+		orange: "border-[#f97316]",
+	} as const;
+
+    const gradeToColour = (grade: Grade): keyof typeof borderColours => {
+        if (grade === "warning") return "yellow";
+        if (grade === "danger") return "orange";
+        return "red";
+    }
+
+    const getTitle = (rec: Recommendation) => {
+        if (rec.type === "UV") return "UV Index Warning"
+        if (rec.type === "air-temperature") return "Temperature Warning"
+        if (rec.type === "ground-temperature") return "Ground Temperature Warning"
+    }
+</script>
+
+<Card.Root class={borderColours[gradeToColour(recommendation.grade)]}>
+    <Card.Header>
+        {#if recommendation.type === "UV"}
+            <Sun strokeColour={getColourForGrade(recommendation.grade)} /> 
+        {:else if recommendation.type === "air-temperature"}
+            <Thermometer strokeColour={getColourForGrade(recommendation.grade)} /> 
+        {:else}
+            <Paw strokeColour={getColourForGrade(recommendation.grade)} />
+        {/if}
+        <h3 class="scroll-m-20 text-2xl font-semibold tracking-tight">{getTitle(recommendation)}</h3>
+        <p class="text-xl text-muted-foreground">{dayjs.unix(recommendation.timeStart).format('h:mm a D MMM')} - {dayjs.unix(recommendation.timeEnd).endOf("hour").format('h:mm a D MMM')}</p>
+    </Card.Header>
+    <Card.Content>
+        {#each getRecommendationMessages(recommendation.grade, recommendation.type) as message}
+            <p>{message}</p>
+        {/each}
+    </Card.Content>
+</Card.Root>
--- src/lib/components/Recommendations.svelte
+++ src/lib/components/Recommendations.svelte
@@ -1,25 +1,15 @@
 <script lang="ts">
-	import { getColourForGrade } from "$lib/colours";
-	import { temperatureLimits } from "$lib/domain/dogSafety";
-	import { getRecommendationMessages } from "$lib/domain/recommendation";
-	import Paw from "$lib/icons/Paw.svelte";
-	import * as Card from "$lib/shadcn/ui/card";
-	import type { ForecastRecord, Grade } from "$lib/types";
-	import dayjs from "dayjs";
+	import { temperatureLimits } from "$lib/domain/recommendation/dogSafety";
+	import { uvIndexLimits } from "$lib/domain/recommendation/humanSafety";
+	import type { Recommendation } from "$lib/domain/recommendation/types";
+	import type { ForecastRecord } from "$lib/types";
+	import RecommendationCard from "./RecommendationCard.svelte";
 
     export let forecasts: ForecastRecord[];
 
     const RECOMMENDATION_GRADES_TO_INCLUDE = ["warning","danger","extreme"];
 
-    type Recommendation = {
-        type: "UV"|"air-temperature"|"ground-temperature";
-        subject: "dog"|"human";
-        grade: Grade;
-        timeStart: number;
-        timeEnd: number;
-    };
-
-    const gradedForecasts = forecasts.map<Recommendation>((forecast) => {
+    const gradedTemperatureForecasts = forecasts.map<Recommendation>((forecast) => {
         if (forecast.temperature < temperatureLimits.safe) {
             return {
                 type: "air-temperature", 
@@ -59,7 +49,47 @@
     .toSorted(({timeStart: a}, {timeStart: b}) => a<b?0:1)
     ;
 
-    const airTemperatureRecommendations = gradedForecasts
+    const gradedUvIndexForecasts = forecasts.map<Recommendation>((forecast) => {
+        if (forecast.uvIndex < uvIndexLimits.safe) {
+            return {
+                type: "UV", 
+                subject: "human",
+                grade: "safe",
+                timeStart: forecast.time,
+                timeEnd: forecast.time,
+            }
+        }
+        if (forecast.uvIndex < uvIndexLimits.warning) {
+            return {
+                type: "UV", 
+                subject: "human",
+                grade: "warning",
+                timeStart: forecast.time,
+                timeEnd: forecast.time,
+            }
+        }
+        if (forecast.uvIndex < uvIndexLimits.danger) {
+            return {
+                type: "UV", 
+                subject: "human",
+                grade: "danger",
+                timeStart: forecast.time,
+                timeEnd: forecast.time,
+            }
+        }
+        return {
+                type: "UV", 
+                subject: "human",
+                grade: "extreme",
+                timeStart: forecast.time,
+                timeEnd: forecast.time,
+            }
+    })
+    .filter(({ grade}) => RECOMMENDATION_GRADES_TO_INCLUDE.includes(grade))
+    .toSorted(({timeStart: a}, {timeStart: b}) => a<b?0:1)
+    ;
+
+    const airTemperatureRecommendations = gradedTemperatureForecasts
         .reduce<Recommendation[]>((acc, cur) => {
             const prev = acc.at(-1);
             if (acc.length === 0 || !prev) {
@@ -72,7 +102,7 @@
             return [...acc, cur];
         }, new Array<Recommendation>());
 
-    const groundTemperatureRecommendations = gradedForecasts
+    const groundTemperatureRecommendations = gradedTemperatureForecasts
         .reduce<Recommendation[]>((acc, cur) => {
             const currentGround: Recommendation = {
                 ...cur, 
@@ -89,38 +119,31 @@
             return [...acc, currentGround];
         }, new Array<Recommendation>());
 
-    const allRecommendations = [...airTemperatureRecommendations, ...groundTemperatureRecommendations]
+    const uvIndexRecommendations = gradedUvIndexForecasts
+        .reduce<Recommendation[]>((acc, cur) => {
+            const currentGround: Recommendation = {
+                ...cur, 
+                type: "UV",
+            };
+            const prev = acc.at(-1);
+            if (acc.length === 0 || !prev) {
+                return [...acc, currentGround];
+            }
+            if (prev.grade === currentGround.grade) {
+                const rest = acc.slice(0,-1);
+                return [...rest, {...prev, timeEnd: currentGround.timeEnd}];
+            }
+            return [...acc, currentGround];
+        }, new Array<Recommendation>());
+
+    const allRecommendations = [...airTemperatureRecommendations, ...groundTemperatureRecommendations, ...uvIndexRecommendations]
         .toSorted(({timeStart: a}, {timeStart: b}) => a<b?0:1);
 
-    //TODO: Cannot dynamically assemble classnames, so this is the current workaround
-	// These colours reference the values in the colours.ts file
-	const borderColours = {
-		red: "border-[#dc2626]",
-		darkBlue : "border-[#4f46e5]",
-		lightBlue: "border-[#0ea5e9]",
-		orange: "border-[#f97316]",
-	} as const;
 
-    const gradeToColour = (grade: Grade): keyof typeof borderColours => {
-        if (grade === "safe") return "darkBlue";
-        if (grade === "warning") return "lightBlue";
-        if (grade === "danger") return "orange";
-        return "red";
-    }
 </script>
 <section class="col-start-1 col-span-2 gap-4 flex flex-col">
-    <h3>Recommendations</h3>
+    <h2 class="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">Recommendations</h2>
     {#each allRecommendations as recommendation}
-        <Card.Root class={borderColours[gradeToColour(recommendation.grade)]}>
-            <Card.Header>
-                <Paw strokeColour={getColourForGrade(recommendation.grade)} />
-                <h4>{dayjs.unix(recommendation.timeStart).format('h:mm a D MMM')} - {dayjs.unix(recommendation.timeEnd).endOf("hour").format('h:mm a D MMM')}</h4>
-            </Card.Header>
-            <Card.Content>
-                {#each getRecommendationMessages(recommendation.grade, recommendation.type) as message}
-                    <p>{message}</p>
-                {/each}
-            </Card.Content>
-        </Card.Root>
+        <RecommendationCard {recommendation} />
     {/each}
 </section>
