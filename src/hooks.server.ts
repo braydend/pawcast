f3da666e8ae59ed60105c88edbd741a1efb00cab
Brayden Di Luca <bray.diluca@estimateone.com>
2023-12-20 14:13:37 +1100

    Add environment to sentry config

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,10 @@
+import { VERCEL_ENV } from '$env/static/private';
 import * as Sentry from '@sentry/sveltekit';
 
 Sentry.init({
 	dsn: 'https://f806b732dba19a52cc1e1539570d76b0@o538041.ingest.sentry.io/4506421221130240',
+
+	environment: VERCEL_ENV,
 
 	tracesSampleRate: 0.3
 });
