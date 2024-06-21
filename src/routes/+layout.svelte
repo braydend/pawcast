9ef76df6fd25fd34587df8cf1c826f7cd8a79984
Brayden Di Luca <bray.diluca@estimateone.com>
2024-06-21 11:29:07 +1000

    Add plausible anayltics script to head

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
-    import "../app.pcss";
-    import { ModeWatcher } from "mode-watcher";
+	import { dev } from '$app/environment';
+	import '../app.pcss';
+	import { ModeWatcher } from 'mode-watcher';
 </script>
+
+<svelte:head>
+	{#if !dev}
+		<script defer data-domain="pawcast.app" src="https://plausible.io/js/script.js"></script>
+	{/if}
+</svelte:head>
+
 <ModeWatcher />
 <slot></slot>
