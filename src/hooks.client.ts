2ece2c2bcc93d4cdc9bf0cfa6896496eb75e09c4
Brayden <braydendiluca@gmail.com>
2024-09-03 19:11:57 +1000

    Fix exposed private env var in client code

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,10 +1,10 @@
-import { VERCEL_ENV } from '$env/static/private';
+import { PUBLIC_VERCEL_ENV } from '$env/static/public';
 import * as Sentry from '@sentry/sveltekit';
 
 Sentry.init({
 	dsn: 'https://f806b732dba19a52cc1e1539570d76b0@o538041.ingest.sentry.io/4506421221130240',
 
-	environment: VERCEL_ENV,
+	environment: PUBLIC_VERCEL_ENV,
 
 	tracesSampleRate: 0.3,
 
